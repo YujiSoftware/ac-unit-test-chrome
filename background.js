24e03d02787a40013dc046eac238751a4c85a2d5
YujiSoftware <yuji.software+github@gmail.com>
2016-04-05 01:53:17 +0900

    Fixed a bug that visible context menu on any page

--- background.js
+++ background.js
@@ -2,7 +2,8 @@ chrome.contextMenus.create({
     "title" : "ユニットテストを生成",
     "type" : "normal",
     "id": "createUnitTest",
-    "contexts" : ["page"]
+    "contexts" : ["page"],
+    "documentUrlPatterns": ["http://*.atcoder.jp/tasks/*"]
 });
 
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
